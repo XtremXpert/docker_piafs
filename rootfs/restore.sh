53f4028760932f09708686ece06b2f5157f8cd39
Benoît Vézina <benoit@vezina.biz>
2017-12-15 11:46:55 -0500

    Update restore.sh

--- rootfs/restore.sh
+++ rootfs/restore.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 mysql -u $DB_USER --password=$DB_PASS -h $DB_HOST $DB_NAME < /backups/dump.sql
 
-duplicity restore file:///backups/galleries/* /var/www/html/galleries/
+duplicity --no-encryption --force file:///backups/galleries /var/www/html/galleries/
 
-duplicity restore file:///backups/themes/* /var/www/html/themes/
+duplicity --no-encryption --force file:///backups/themes /var/www/html/themes/
 
-duplicity restore file:///backups/local/* /var/www/html/local/
+duplicity --no-encryption --force file:///backups/local /var/www/html/local/
 
-duplicity restore file:///backups/plugins/* /var/www/html/plugins/
+duplicity --no-encryption --force file:///backups/plugins /var/www/html/plugins/
 
-duplicity restore file:///backups/upload/* /var/www/html/upload/
+duplicity --no-encryption --force file:///backups/upload /var/www/html/upload/
 
